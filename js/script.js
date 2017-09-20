4b8ab7dbc7bd86e27e7ffc0e41dc0764e8c9ae1a
Viktoria1592 <92viktori@mail.ru>
2017-09-20 23:02:56 +0300

    верстка главного меню

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-var popup = document.querySelector (".pop-up");
-var write = document.querySelector (".btn-write");
-var close = popup.querySelector (".close");
-var yourName = popup.querySelector ("[name=name]");
-var yourEmail = popup.querySelector ("[name=email]");
+var popup = document.querySelector(".pop-up");
+var write = document.querySelector(".btn-write");
+var close = popup.querySelector(".close");
+var login = popup.querySelector("#yourname");
+var email = popup.querySelector("#youremail");
 var form = popup.querySelector('form');
-var modalOverlay = document.querySelector (".overlay");
+var modalOverlay = document.querySelector(".overlay");
 var storage = localStorage.getItem('yourName');
 
 
-if (write) write.addEventListener ("click", function(event) {
-event.preventDefault();
-popup.classList.add("pop-up-active");
-modalOverlay.classList.add("overlay-active");
-if (storage) 
-{yourEmail.focus();
-yourName.value = storage;
-} 
-else {yourName.focus();}
+write.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.add("pop-up-active");
+    modalOverlay.classList.add("overlay-active");
+    if (storage) {
+        login.value = storage;
+        email.focus();
+    } else {
+        login.focus();
+    }
 });
 
-if (close) close.addEventListener ("click", function(event) {
-event.preventDefault();
-popup.classList.remove("pop-up-active");
-modalOverlay.classList.remove("overlay-active");
-popup.classList.remove("pop-up-error");
+close.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.remove("pop-up-active");
+    modalOverlay.classList.remove("overlay-active");
+    popup.classList.remove("pop-up-error");
 
 });
 
 form.addEventListener('submit', function (event) {
-    if (!yourName.value || !yourEmail.value) {
+    if (!login.value || !email.value) {
         event.preventDefault();
         popup.classList.add('pop-up-error');
     } else {
-        localStorage.setItem('yourName', yourName.value);
+        localStorage.setItem('login', login.value);
     }
 });
 
@@ -52,22 +53,28 @@ window.addEventListener('keydown', function (event) {
     }
 });
 ymaps.ready(initMap);
+
 function initMap() {
-  var myMap;
+    var myMap;
     myMap = new ymaps.Map("map", {
         center: [59.938816, 30.323244],
         zoom: 16,
         controls: []
     });
     myMap.behaviors.disable('scrollZoom');
- 
+
     myMap.controls.add("zoomControl", {
-        position: {top: 15, left: 15}
+        position: {
+            top: 15,
+            left: 15
+        }
     });
-  var myPlacemark = new ymaps.Placemark([59.938816, 30.323244] , {}, 
-        { iconLayout: 'default#image',
-          iconImageHref: ('img/marker.svg'),
-          iconImageSize: [218, 142],
-          iconImageOffset: [-20, -47] });     
- 
-    myMap.geoObjects.add(myPlacemark);}
+    var myPlacemark = new ymaps.Placemark([59.938816, 30.323244], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: ('img/marker.svg'),
+        iconImageSize: [218, 142],
+        iconImageOffset: [-20, -47]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
